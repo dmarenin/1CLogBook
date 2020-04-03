5c5827066dc5b5c8ea167c16fb870e6af5770d40
dmarenin <39666107+dmarenin@users.noreply.github.com>
2020-04-03 11:24:50 +0500

    update back end

--- python/api_log.py
+++ python/api_log.py
@@ -1,29 +1,31 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
-import threading
-import json
-from urllib.parse import urlparse, parse_qs
-import socket
-import api_log_func
 
 class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        from urllib.parse import urlparse, parse_qs
+        import json
+        import api_log_func
 
         path = urlparse(self.path).path
+        
         qs = urlparse(self.path).query
         qs = parse_qs(qs)
 
         res =  api_log_func.callback(path, qs, self)
 
-        resp = json.dumps(res, default=api_log_func.json_serial).encode()
+        resp = json.dumps(res, default=json_serial).encode()
 
         self.send_response(200)
+        
         self.send_header("Content-type", "application/json")
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Expose-Headers", "Access-Control-Allow-Origin")
         self.send_header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+        
         self.end_headers()
+        
         self.wfile.write(resp)
         
         return
@@ -31,7 +33,16 @@ class Handler(BaseHTTPRequestHandler):
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
+
+def json_serial(obj):
+    from datetime import datetime, timedelta
+    if isinstance(obj, (datetime, datetime.date)):
+        return obj.isoformat()
+    raise TypeError("Type is not serializable %s" % type(obj))
+
+
 if __name__ == '__main__':
     server = ThreadedHTTPServer(('0.0.0.0', 8010), Handler)
     print('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
+
--- python/api_log_func.py
+++ python/api_log_func.py
@@ -1,237 +1,232 @@
-#import peewee
+import pymssql
 
-#import json
-from datetime import datetime
-from datetime import timedelta
-
-#import os
-#import orm.fields
-#import gevent
-#import threading
-## from call import Call # TODO loopback!
-
-#import inspect
-
-## import asyncio
-
-import pymssql, base64, sys, json, time
 
 def get_mssql_conn():
+    conn = pymssql.connect(server='', user='sa', password='', database='ExLogBook2')  
     
-    conn = pymssql.connect(server='itts', user='sa', password='Aa123456', database='ExLogBook2')  
-    cursor = conn.cursor()  
+    return conn.cursor()
 
-    return cursor
+def callback(path, qs, self):
+    return eval('%s(qs)' % path[1:])
 
-def api_log_get_meta_data(qs):
-   
-    #res = (OrgCRM
-    #    .select(OrgCRM.ref, OrgCRM.name)
-    #    .where(OrgCRM.mark == False)
-    #    .where(OrgCRM.show == True))
-
-    #res = res.dicts()
-    #return list(res) 
-   
+def api_log_get_db(qs):
     d = []
 
     cursor = get_mssql_conn()
 
-    cursor.execute('SELECT [Code],[Name] FROM [ExLogBook2].[dbo].[Metadata] WHERE [InfobaseCode]=1;')  
-
+    cursor.execute('SELECT [Code], [Name] FROM [ExLogBook2].[dbo].[Infobases];')
+    
     row = cursor.fetchone()  
     while row:  
-        
-        d.append({'code':row[0],'name':row[1]})
+        d.append({'code':row[0], 'name':row[1]})
 
         row = cursor.fetchone()  
 
-    #return sorted(d, key=lambda e: e['name'])
+    return sorted(d, key=lambda e: e['name'])
+
+def api_log_get_meta_data(qs):
+    d = []
+
+    cursor = get_mssql_conn()
+
+    cursor.execute(f"""SELECT [Code], [Name] FROM [ExLogBook2].[dbo].[Metadata] WHERE [InfobaseCode]={qs['db'][0]};""")  
+    row = cursor.fetchone()  
+    while row:  
+        d.append({'code':row[0], 'name':row[1]})
+
+        row = cursor.fetchone()  
+
     return d
 
 def api_log_get_event_objs(qs):
-    
-    #in:
-    #metadata_id
-    #data_string
-     
+    db = 1
+    if not qs.get('db') is None:
+        db = qs['db'][0]
+
+    data_string = ''
+    if not qs.get('data_string') is None:
+        data_string = ' AND [DataString] LIKE \'%'+qs.get('data_string')[0]+'%\''
+
+    metadata_id = ''
+    if not qs.get('metadata_id') is None:
+        metadata_id = ' AND [MetadataId] = '+qs.get('metadata_id')[0]
+
+    date_start = ''
+    if not qs.get('date_start') is None:
+        date_start = ' AND [DateTime] >= CONVERT(DATETIME, \''+qs.get('date_start')[0]+' 00:00\', 103)'
+
+    date_end = ''
+    if not qs.get('date_end') is None:
+        date_end = ' AND [DateTime] <= CONVERT(DATETIME, \''+qs.get('date_end')[0]+' 23:59\', 103)'
+
     d = []
 
-    query_text = 'SELECT DISTINCT [DataString], [DataStructure], [ref_ones] FROM [ExLogBook2].[dbo].[v_Events] WHERE [InfobaseCode]=1 %data_string% %metadata_id% %date_start% %date_end%'
+    query_text = f"""SELECT DISTINCT [DataString], [DataStructure], [ref_ones] FROM [ExLogBook2].[dbo].[v_Events] WHERE [InfobaseCode]={db} {data_string} {metadata_id} {date_start} {date_end}"""
 
-    if qs.get('data_string'):
-        query_text = query_text.replace('%data_string%', ' AND [DataString] LIKE \'%'+qs.get('data_string')[0]+'%\'')
-    else:
-        query_text = query_text.replace('%data_string%', '')
-        
-    if qs.get('metadata_id'):
-        query_text = query_text.replace('%metadata_id%', ' AND [MetadataId] = '+qs.get('metadata_id')[0])
-    else:
-        query_text = query_text.replace('%metadata_id%', '')
-        
-    if qs.get('date_start'):
-        query_text = query_text.replace('%date_start%', ' AND [DateTime] >= CONVERT(DATETIME, \''+qs.get('date_start')[0]+' 00:00\', 103)')
-    else:
-        query_text = query_text.replace('%date_start%', '')
-            
-    if qs.get('date_end'):
-        query_text = query_text.replace('%date_end%', ' AND [DateTime] <= CONVERT(DATETIME, \''+qs.get('date_end')[0]+' 23:59\', 103)')
-    else:
-        query_text = query_text.replace('%date_end%', '')
-    
     cursor = get_mssql_conn()
 
     cursor.execute(query_text)  
+
     row = cursor.fetchone()  
     while row:  
-        
-        d.append(
-            {
-                #'date_time':'',
-                #'comment':row[1],
-                'data_string':row[0],
-                'data_structure':row[1],
-                #'user_name':row[4],
-                #'computer_name':row[5]
-                'ref_ones':row[2]
-             }
-            )
+        d.append({'data_string':row[0], 'data_structure':row[1], 'ref_ones':row[2]})
 
         row = cursor.fetchone()  
 
     return sorted(d, key=lambda e: e['data_string'])
 
 def api_log_get_results(qs):
+    db = 4
+    if not qs.get('db') is None:
+        db = qs['db'][0]
+
+    data_string = ''
+    if not qs.get('data_string') is None:
+        data_string = ' AND [DataString] LIKE \'%'+qs.get('data_string')[0]+'%\''
+
+    metadata_id = ''
+    if not qs.get('metadata_id') is None:
+        metadata_id = ' AND [MetadataId] = '+qs.get('metadata_id')[0]
+
+    date_start = ''
+    if not qs.get('date_start') is None:
+        date_start = ' AND [DateTime] >= CONVERT(DATETIME, \''+qs.get('date_start')[0]+' 00:00\', 103)'
+
+    date_end = ''
+    if not qs.get('date_end') is None:
+        date_end = ' AND [DateTime] <= CONVERT(DATETIME, \''+qs.get('date_end')[0]+' 23:59\', 103)'
+
+    data_structure = ''
+    if not qs.get('data_structure') is None:
+        data_structure = ' AND [DataStructure] = '+qs.get('data_structure')[0]
    
-    #in:
-    #data_structure
-    #data_string    
-    ##data_string
+    ref_ones = ''
+    if not qs.get('ref_ones') is None:
+        ref_ones = ' AND [ref_ones] = \''+qs.get('ref_ones')[0]+'\''
 
     d = []
 
-    query_text = 'SELECT [DateTime], [Comment], [DataString], [DataStructure], [UsersName], [ComputerName], [MetadataName], [EventTypeName], [ApplicationName], [EventType], [EventID], [TransactionStatus] FROM [ExLogBook2].[dbo].[v_Events] WHERE [InfobaseCode]=1 %data_structure% %data_string% %metadata_id% %ref_ones% %date_start% %date_end%'
+    query_text = f"""SELECT [DateTime], [Comment], [DataString], [DataStructure], [UsersName], [ComputerName], [MetadataName], [EventTypeName], [ApplicationName], [EventType], [EventID], [TransactionStatus] FROM [ExLogBook2].[dbo].[v_Events] WHERE [InfobaseCode]={db} {data_structure} {data_string} {metadata_id} {ref_ones} {date_start} {date_end}"""
 
-    if qs.get('data_string'):
-        query_text = query_text.replace('%data_string%', ' AND [DataString] LIKE \'%'+qs.get('data_string')[0]+'%\'')
-    else:
-        query_text = query_text.replace('%data_string%', '')
-    
-    if qs.get('data_structure'):
-        query_text = query_text.replace('%data_structure%', ' AND [DataStructure] = '+qs.get('data_structure')[0])
-    else:
-        query_text = query_text.replace('%data_structure%', '')         
-        
-    if qs.get('metadata_id'):
-        query_text = query_text.replace('%metadata_id%', ' AND [MetadataId] = '+qs.get('metadata_id')[0])
-    else:
-        query_text = query_text.replace('%metadata_id%', '')
-    
-    if qs.get('ref_ones'):
-        query_text = query_text.replace('%ref_ones%', ' AND [ref_ones] = \''+qs.get('ref_ones')[0]+'\'')
-    else:
-        query_text = query_text.replace('%ref_ones%', '') 
-    
-    if qs.get('date_start'):
-        query_text = query_text.replace('%date_start%', ' AND [DateTime] >= CONVERT(DATETIME, \''+qs.get('date_start')[0]+' 00:00\', 103)')
-    else:
-        query_text = query_text.replace('%date_start%', '')
-            
-    if qs.get('date_end'):
-        query_text = query_text.replace('%date_end%', ' AND [DateTime] <= CONVERT(DATETIME, \''+qs.get('date_end')[0]+' 23:59\', 103)')
-    else:
-        query_text = query_text.replace('%date_end%', '')
-    
     cursor = get_mssql_conn()
 
-    cursor.execute(query_text)  
+    cursor.execute(query_text) 
+    
     row = cursor.fetchone()  
     while row:  
-        
-        d.append(
-            {
-                'date_time':row[0],
-                'comment':row[1],
-                'data_string':row[2],
-                'data_structure':row[3],
-                'user_name':row[4],
-                'computer_name':row[5],
-                'metadata_name':row[6],
-                'event_type_name':row[7],
-                'application_name':row[8],
-                'event_type':row[9],
-                'event_id':row[10],
-                'transaction_status':row[11]
-             }
-            )
+        d.append({'date_time':row[0], 'comment':row[1], 'data_string':row[2], 'data_structure':row[3], 'user_name':row[4], 'computer_name':row[5], 'metadata_name':row[6], 'event_type_name':row[7], 'application_name':row[8], 'event_type':row[9], 'event_id':row[10], 'transaction_status':row[11]})
 
         row = cursor.fetchone()  
 
     return sorted(d, key=lambda e: e['date_time'])
 
-def callback(path, qs, self):
-
-    res = eval('%s(qs)' % path[1:])
-
-    return res
-
 def api_log_get_result_mod_zn(qs):
-   
-    #in:
-    #data_structure
-    #data_string    
-    ##data_string
-
     d = []
 
-    query_text = 'SELECT DISTINCT DataString, UsersName, mod_post_doc, ref_ones, var_1, var_2, var_3, var_4 FROM (SELECT [DateTime], [Comment], [DataString], [DataStructure], [UsersName], [ComputerName], [MetadataName], [EventTypeName], [ApplicationName], [EventType], [EventID], [TransactionStatus], CASE WHEN [Comment] like \'%'+'Статус проведения документа до изменения:Да'+'%\' THEN 1 ELSE 0 END AS mod_post_doc, [ref_ones], CASE WHEN [Comment] like \'%<Контрагент>:%\' AND [Comment] like \'%<Автомобиль>:%\'THEN 1 ELSE 0 END AS var_1, CASE WHEN [Comment] like \'%<Автомобиль>:%\'THEN 1 ELSE 0 END AS var_2, CASE WHEN [Comment] like \'%<Контрагент>:%\'THEN 1 ELSE 0 END AS var_3, CASE WHEN [Comment] like \'%Таблица товаров до записи:%\' THEN 1 ELSE 0 END AS var_4 FROM [ExLogBook2].[dbo].[v_Events] WHERE [InfobaseCode]=1 and ([Comment] like \'%'+'Измененнные реквизиты шапки до записи:'+'%\' or [Comment] like \'%+'+'Таблица товаров до записи:'+ '%\' or [Comment] like \'%'+'Статус проведения документа до изменения:%\') %metadata_id% %date_start% %date_end% ) AS T1 WHERE (T1.mod_post_doc=0 and (T1.var_1=1 or T1.var_2=1 or T1.var_3=1)) or (T1.mod_post_doc=1 and T1.var_4=1)  order by var_1 desc, var_2 desc, var_3 desc, var_4 desc '
+    query_text = 'SELECT DISTINCT DataString, UsersName, mod_post_doc, ref_ones, var_1, var_2, var_3, var_4 FROM (SELECT [DateTime], [Comment], [DataString], [DataStructure], [UsersName], [ComputerName], [MetadataName], [EventTypeName], [ApplicationName], [EventType], [EventID], [TransactionStatus], CASE WHEN [Comment] like \'%'+'Статус проведения документа до изменения:Да'+'%\' THEN 1 ELSE 0 END AS mod_post_doc, [ref_ones], CASE WHEN [Comment] like \'%<Контрагент>:%\' AND [Comment] like \'%<Автомобиль>:%\'THEN 1 ELSE 0 END AS var_1, CASE WHEN [Comment] like \'%<Автомобиль>:%\'THEN 1 ELSE 0 END AS var_2, CASE WHEN [Comment] like \'%<Контрагент>:%\'THEN 1 ELSE 0 END AS var_3, CASE WHEN [Comment] like \'%Таблица товаров до записи:%\' THEN 1 ELSE 0 END AS var_4 FROM [ExLogBook2].[dbo].[v_Events] WHERE [InfobaseCode]=4 and ([Comment] like \'%'+'Измененнные реквизиты шапки до записи:'+'%\' or [Comment] like \'%+'+'Таблица товаров до записи:'+ '%\' or [Comment] like \'%'+'Статус проведения документа до изменения:%\') %metadata_id% %date_start% %date_end% ) AS T1 WHERE (T1.mod_post_doc=0 and (T1.var_1=1 or T1.var_2=1 or T1.var_3=1)) or (T1.mod_post_doc=1 and T1.var_4=1)  order by var_1 desc, var_2 desc, var_3 desc, var_4 desc '
 
-    query_text = query_text.replace('%metadata_id%', ' AND [MetadataId] = 12')
+    query_text = query_text.replace('%metadata_id%', ' AND [MetadataId] = 120')
 
+    date_start = ''
     if qs.get('date_start'):
-        query_text = query_text.replace('%date_start%', ' AND [DateTime] >= CONVERT(DATETIME, \''+qs.get('date_start')[0]+' 00:00\', 103)')
-    else:
-        query_text = query_text.replace('%date_start%', '')
-            
+        date_start =' AND [DateTime] >= CONVERT(DATETIME, \''+qs.get('date_start')[0]+' 00:00\', 103)'
+
+    query_text = query_text.replace('%date_start%', date_start)
+    
+    date_end = ''
     if qs.get('date_end'):
-        query_text = query_text.replace('%date_end%', ' AND [DateTime] <= CONVERT(DATETIME, \''+qs.get('date_end')[0]+' 23:59\', 103)')
-    else:
-        query_text = query_text.replace('%date_end%', '')
+       date_end = ' AND [DateTime] <= CONVERT(DATETIME, \''+qs.get('date_end')[0]+' 23:59\', 103)'
+        
+    query_text = query_text.replace('%date_end%', date_end)
     
     cursor = get_mssql_conn()
 
-    cursor.execute(query_text)  
+    cursor.execute(query_text) 
+    
     row = cursor.fetchone()  
     while row:  
-        
-        d.append(
-            {
-                #'date_time':row[0],
-                #'comment':row[1],
-                'data_string':row[0],
-                #'data_structure':row[3],
-                'user_name':row[1],
-                #'computer_name':row[5],
-                #'metadata_name':row[6],
-                #'event_type_name':row[7],
-                #'application_name':row[8],
-                #'event_type':row[9],
-                #'event_id':row[10],
-                #'transaction_status':row[11],
-                'mod_post_doc':row[2],
-                'ref_ones':row[3],
-                'var_1':row[4],
-                'var_2':row[5],
-                'var_3':row[6],
-                'var_4':row[7]
-             }
-            )
+        d.append({'data_string':row[0], 'user_name':row[1], 'mod_post_doc':row[2], 'ref_ones':row[3], 'var_1':row[4], 'var_2':row[5], 'var_3':row[6], 'var_4':row[7]})
 
         row = cursor.fetchone()  
 
     return d
 
-def json_serial(obj):
-  if isinstance(obj, (datetime, datetime.date)):
-    return obj.isoformat()
-  raise TypeError("Type is not serializable %s" % type(obj))
+def api_log_get_users(qs):
+    d = []
+
+    cursor = get_mssql_conn()
+
+    cursor.execute(f"""SELECT [InfobaseCode] ,[Code] ,[Name] ,[Guid] FROM [ExLogBook2].[dbo].[Users] WHERE [InfobaseCode]={qs['db'][0]} ORDER BY [Name]""")  
+    row = cursor.fetchone()  
+    while row:  
+        d.append({'code':row[1], 'name':row[2]})
+
+        row = cursor.fetchone()  
+
+    return d
+
+def api_log_get_events(qs):
+    d = []
+
+    cursor = get_mssql_conn()
+
+    cursor.execute(f"""SELECT [InfobaseCode], [Code], 
+                        
+     CASE WHEN dbo.EventsType.Name = '_$Session$_.Start' THEN 'Сеанс. Начало' WHEN dbo.EventsType.Name = '_$Session$_.Finish' THEN 'Сеанс. Завершение' WHEN dbo.EventsType.Name = '_$InfoBase$_.ConfigUpdate' THEN
+                          'Информационная база. Изменение конфигурации' WHEN dbo.EventsType.Name = '_$InfoBase$_.DBConfigUpdate' THEN 'Информационная база. Изменение конфигурации  базы данных' WHEN dbo.EventsType.Name =
+                          '_$InfoBase$_.EventLogSettingsUpdate' THEN 'Информационная база. Изменение  параметров журнала регистрации' WHEN dbo.EventsType.Name = '_$InfoBase$_.InfoBaseAdmParamsUpdate' THEN 'Информационная база. Изменение  параметров информационной базы'
+                          WHEN dbo.EventsType.Name = '_$InfoBase$_.MasterNodeUpdate' THEN 'Информационная база. Изменение главного  узла' WHEN dbo.EventsType.Name = '_$InfoBase$_.RegionalSettingsUpdate' THEN 'Информационная база. Изменение  региональных установок'
+                          WHEN dbo.EventsType.Name = '_$InfoBase$_.TARInfo' THEN 'Тестирование и исправление. Сообщение' WHEN dbo.EventsType.Name = '_$InfoBase$_.TARMess' THEN 'Тестирование и исправление. Предупреждение' WHEN
+                          dbo.EventsType.Name = '_$InfoBase$_.TARImportant' THEN 'Тестирование и исправление. Ошибка' WHEN dbo.EventsType.Name = '_$Data$_.New' THEN 'Данные. Добавление' WHEN dbo.EventsType.Name = '_$Data$_.Update'
+                          THEN 'Данные. Изменение' WHEN dbo.EventsType.Name = '_$Data$_.Delete' THEN 'Данные. Удаление' WHEN dbo.EventsType.Name = '_$Data$_.TotalsPeriodUpdate' THEN 'Данные. Изменение периода рассчитанных итогов'
+                          WHEN dbo.EventsType.Name = '_$Data$_.Post' THEN 'Данные. Проведение' WHEN dbo.EventsType.Name = '_$Data$_.Unpost' THEN 'Данные. Отмена проведения' WHEN dbo.EventsType.Name = '_$User$_.New' THEN 'Пользователи. Добавление'
+                          WHEN dbo.EventsType.Name = '_$User$_.Update' THEN 'Пользователи. Изменение' WHEN dbo.EventsType.Name = '_$User$_.Delete' THEN 'Пользователи. Удаление' WHEN dbo.EventsType.Name = '_$Job$_.Start' THEN
+                          'Фоновое задание. Запуск' WHEN dbo.EventsType.Name = '_$Job$_.Succeed' THEN 'Фоновое задание. Успешное завершение' WHEN dbo.EventsType.Name = '_$Job$_.Fail' THEN 'Фоновое задание. Ошибка выполнения'
+                          WHEN dbo.EventsType.Name = '_$Job$_.Cancel' THEN 'Фоновое задание. Отмена' WHEN dbo.EventsType.Name = '_$PerformError$_' THEN 'Ошибка выполнения' WHEN dbo.EventsType.Name = '_$Transaction$_.Begin' THEN
+                          'Транзакция. Начало' WHEN dbo.EventsType.Name = '_$Transaction$_.Commit' THEN 'Транзакция. Фиксация' WHEN dbo.EventsType.Name = '_$Transaction$_.Rollback' THEN 'Транзакция. Отмена' WHEN dbo.EventsType.Name
+                          = '_$Access$_.Access' THEN 'Объект.Доступ' ELSE dbo.EventsType.Name end FROM [ExLogBook2].[dbo].[EventsType] WHERE [InfobaseCode]={qs['db'][0]} """)  
+    row = cursor.fetchone()  
+    while row:  
+        d.append({'code':row[1], 'name':row[2]})
+
+        row = cursor.fetchone()  
+
+    return d
+
+def api_log_get_user_results(qs):
+    db = 4
+    if not qs.get('db') is None:
+        db = qs['db'][0]
+
+    event = ''
+    if not qs.get('event') is None:
+        if not qs.get('event')[0]=='0':
+            event = ' AND [EventID] = '+qs.get('event')[0]
+
+    user = ''
+    if not qs.get('user') is None:
+        user = f""" AND [UsersName] = '{qs.get('user')[0]}'"""
+
+    date_start = ''
+    if not qs.get('date_start') is None:
+        date_start = ' AND [DateTime] >= CONVERT(DATETIME, \''+qs.get('date_start')[0]+' 00:00\', 103)'
+
+    date_end = ''
+    if not qs.get('date_end') is None:
+        date_end = ' AND [DateTime] <= CONVERT(DATETIME, \''+qs.get('date_end')[0]+' 23:59\', 103)'
+
+    d = []
+
+    query_text = f"""SELECT [DateTime], [Comment], [DataString], [DataStructure], [UsersName], [ComputerName], [MetadataName], [EventTypeName], [ApplicationName], [EventType], [EventID], [TransactionStatus] FROM [ExLogBook2].[dbo].[v_Events] WHERE [InfobaseCode]={db} {event} {user} {date_start} {date_end}"""
+
+    cursor = get_mssql_conn()
+
+    cursor.execute(query_text) 
+    
+    row = cursor.fetchone()  
+    while row:  
+        d.append({'date_time':row[0], 'comment':row[1], 'data_string':row[2], 'data_structure':row[3], 'user_name':row[4], 'computer_name':row[5], 'metadata_name':row[6], 'event_type_name':row[7], 'application_name':row[8], 'event_type':row[9], 'event_id':row[10], 'transaction_status':row[11]})
+
+        row = cursor.fetchone()  
+
+    return sorted(d, key=lambda e: e['date_time'])
